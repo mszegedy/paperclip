3ebdc180d79b9de6c7918f61e956996306bc7d3a
Maria Szegedy <mszegedy2@gmail.com>
2017-10-23 03:22:12 -0400

    Added barebones functionality.

--- paperclip.py
+++ paperclip.py
@@ -12,13 +12,13 @@ interest. Most plotting commands are based on Matlab commands.
 '''
 
 import os, sys
-import fcntl, hashlib, json, subprocess, time
+import fcntl, hashlib, json, time
 import cmd
 import pyrosetta as pr
 import mszpyrosettaextension as mpre
 from pyrosetta.rosetta.core.scoring import all_atom_rmsd
 import matplotlib
-# matplotlib.use("Agg") # otherwise lack of klab display breaks program
+matplotlib.use("Agg") # otherwise lack of display breaks program
 import matplotlib.pyplot as plt
 
 PYROSETTA_ENV = None
@@ -54,7 +54,7 @@ class PyRosettaEnv():
         self.initp = False
         self.scorefxn = None
     @needs_pr_init
-    def set_scorefxn(name=None, patch=None):
+    def set_scorefxn(self, name=None, patch=None):
         """Sets the scorefxn and optionally applies a patch. Defaults to
         get_fa_scorefxn() with no arguments."""
         if name is None:
@@ -68,3 +68,135 @@ class PyRosettaEnv():
 
 class OurCmdLine(cmd.Cmd):
     """Singleton class for our interactive CLI."""
+    ## Built-in vars
+    intro = 'Welcome to PAPERCLIP. Type help or ? to list commands.'
+    prompt = '* '
+    ## Our vars (don't wanna mess with __init__)
+    cmdfile = None
+    settings = {'calculation': True,
+                'caching': True,
+                'plotting': True}
+
+    ## Housekeeping
+    def do_quit(self, arg):
+        """Stop recording and exit:  quit"""
+        self.close()
+        return True
+    def do_EOF(self, arg):
+        """Stop recording and exit:  EOF  |  ^D"""
+        return self.do_quit(arg)
+    def emptyline(self):
+        pass
+
+    ## Recording and playing back commands
+    def do_record(self, arg):
+        """Save future commands to filename:  record plot.cmd"""
+        self.cmdfile = open(arg, 'w')
+    def do_playback(self, arg):
+        """Play back commands from a file:  playback plot.cmd"""
+        self.close()
+        with open(arg) as f:
+            self.cmdqueue.extend(f.read().splitlines())
+    def precmd(self, line):
+        if self.cmdfile and 'playback' not in line:
+            print(line, file=self.cmdfile)
+        return line
+    def close(self):
+        if self.cmdfile:
+            self.cmdfile.close()
+            self.cmdfile = None
+
+    ## Shell stuff
+    def do_shell(self, arg):
+        """Call a shell command:  shell cd dir  |  !cd dir"""
+        os.system(arg)
+    def do_cd(self, arg):
+        """Change the current working directory:  cd dir"""
+        try:
+            os.chdir(arg)
+        except FileNotFoundError:
+            print('No such file or directory: ' + arg)
+    def do_mv(self, arg):
+        """Call the shell command mv:  mv a b"""
+        self.do_shell('mv ' + arg)
+    def do_rm(self, arg):
+        """Call the shell command rm:  rm a"""
+        self.do_shell('rm ' + arg)
+    def do_ls(self, arg):
+        """Call the shell command ls:  ls .."""
+        self.do_shell('ls ' + arg)
+    def do_pwd(self, arg):
+        """Get the current working directory:  pwd"""
+        os.getcwd()
+
+    ## Settings
+    def do_get_settings(self, arg):
+        """Print settings of current session:  get_settings"""
+        for key, value in self.settings.items():
+            transformed_value = 'yes' if value == True else \
+                                'no'  if value == False else value
+            print('{0:<12}{1:>8}'.format(key+':', transformed_value))
+    def do_set(self, arg):
+        """Set or toggle a setting variable in the current session:
+  set calculation no  |  set calculation"""
+        args = arg.split()
+        varname  = None
+        varvalue = None
+        if len(args) == 2:
+            varname  = args[0]
+            varvalue = args[1]
+        elif len(args) == 1:
+            varname = args[0]
+        else:
+            print('Incorrect command usage. Try \'help set\'.')
+        if varvalue is None:
+            try:
+                self.settings[varname] = not self.settings[varname]
+            except KeyError:
+                print('That\'s not a valid setting name. Try '
+                      '\'get_settings\'.')
+                return
+        else:
+            value = None
+            if str.lower(varvalue) in ('yes','y'):
+                value = True
+            elif str.lower(varvalue) in ('no','n'):
+                value = False
+            else:
+                print('That\'s not a valid setting value. Try \'yes\' or'
+                      '\'no\'.')
+                return
+            if varname in self.settings.keys():
+                self.settings[varname] = value
+            else:
+                print('That\'s not a valid setting name. Try '
+                      '\'get_settings\'.')
+                return
+
+    ## Basic Rosetta stuff
+    def do_get_scorefxn(self, arg):
+        """Print the name of the current scorefxn, if any:  get_scorefxn"""
+        global PYROSETTA_ENV
+        if PYROSETTA_ENV.scorefxn is None:
+            print('No scorefxn currently set.')
+        else:
+            print(PYROSETTA_ENV.scorefxn.get_name())
+    def do_set_scorefxn(self, arg):
+        """Set the current scorefxn, optionally applying a patchfile:
+    set_scorefxn ref2015  |  set_scorefxn ref2015 docking"""
+        global PYROSETTA_ENV
+        args = arg.split()
+        name = None
+        patch = None
+        if len(args) == 2:
+            name = args[0]
+            patch = args[1]
+        elif len(args) == 1:
+            name = args[0]
+        else:
+            print('Incorrect command usage. Try \'help set_scorefxn\'')
+        PYROSETTA_ENV.set_scorefxn(name=name, patch=patch)
+
+if __name__ == "__main__":
+    PYROSETTA_ENV = PyRosettaEnv()
+    OurCmdLine().cmdloop()
